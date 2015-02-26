9e70fa0b569294a28981e97f34738c9198ec0839
randy <R_Connelie@hotmail.com>
2015-02-26 07:10:01 +0000

    Scraped SnapShowMagic.com with HHTrack so it can be migrated away from Wordpress.

--- wp-content/themes/blue-rooster/scripts/js/slider.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-    $('a.menu_class').click(function () {
-	$('div#login-form').slideToggle('medium');
-    });
-});
--- wp-content/themes/blue-rooster/scripts/rotate/rotate.js
+++
@@ -1,3 +0,0 @@
-  $(function() {
-                $('#rotate > ul').tabs({ fx: { opacity: 'toggle' } }).tabs('rotate', 5000);
-            });
