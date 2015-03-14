cef9e9b6c02c65e47273d60d154b946640c8ef2d
randy <randy@d4c4d6a3-d3ca-4a6b-8eee-ae4fafe91092>
2015-03-14 21:15:59 +0000

    Removing obsolete SQLite database file. Adding migration tools. Fleshed out Gallery model migration.

--- Website/branches/rails/db/migrate/20150314071609_create_galleries.rb
+++ Website/branches/rails/db/migrate/20150314071609_create_galleries.rb
@@ -1,6 +1,11 @@
 class CreateGalleries < ActiveRecord::Migration
   def change
     create_table :galleries do |t|
+			t.string	'title'
+			t.date		'event_date'
+			t.string	'album_url'
+			t.string	'smugmug_album_id'
+			t.string	'smugmug_album_key'
 
       t.timestamps null: false
     end
