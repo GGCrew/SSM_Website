0eb83d76f265a993d688bbedc4633a18d27ba33a
randy <randy@d4c4d6a3-d3ca-4a6b-8eee-ae4fafe91092>
2015-03-16 19:23:28 +0000

    Added formatting to menu items.  Time to work on changing the color scheme.

--- Website/branches/rails/app/controllers/application_controller.rb
+++ Website/branches/rails/app/controllers/application_controller.rb
@@ -5,9 +5,21 @@ class ApplicationController < ActionController::Base
 
 	helper_method :current_user
 
+	before_action :set_selected_menu_item	
+
 	private
 
 	def current_user
 		@current_user ||= User.find(session[:user_id]) if session[:user_id]
+	end
+
+	def set_selected_menu_item
+		@selected_menu_item = case params[:controller]
+			when 'site'
+				params[:action]
+					
+			else
+				params[:controller]
+		end
 	end
 end
