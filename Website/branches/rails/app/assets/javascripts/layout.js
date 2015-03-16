01f3075c1c9ec000482dfc1cf37d243078505820
randy <randy@d4c4d6a3-d3ca-4a6b-8eee-ae4fafe91092>
2015-03-16 18:29:58 +0000

    Added minor animation to banner text.

--- Website/branches/rails/app/assets/javascripts/layout.js
+++ Website/branches/rails/app/assets/javascripts/layout.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
 	var banner_text = $('#head .banner .text .animate');
 	$(banner_text).hide();
+	$(banner_text).css('top', '.5em');
 	$(banner_text).each(function(index, item) {
-		$(item).delay((index + 1) * 1000).fadeIn({
-			duration: 'slow',
-			easing: 'linear',
-			queue: true
-		});
+		$(item)
+			.delay((index + 1) * 1000)
+			.animate({
+				top: '0em',
+				opacity: 'toggle'
+			}, {
+				duration: 'slow',
+				easing: 'swing',
+				queue: true
+			})
 	});
 });
