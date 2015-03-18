9bf8c117435c26dc7e3ed83f031cca03e4d77a8e
randy <randy@d4c4d6a3-d3ca-4a6b-8eee-ae4fafe91092>
2015-03-18 01:22:18 +0000

    Commenting out the "Pricing" menu item.

--- Website/branches/rails/db/schema.rb
+++ Website/branches/rails/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150314222524) do
+ActiveRecord::Schema.define(version: 20150317000001) do
 
   create_table "galleries", force: :cascade do |t|
     t.string   "title",             limit: 255
