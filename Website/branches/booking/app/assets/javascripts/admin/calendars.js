01a40493197439ad54f86381dddc69bab5f9ebfa
randy <randy@d4c4d6a3-d3ca-4a6b-8eee-ae4fafe91092>
2015-08-05 04:24:20 +0000

    Core components of FullCalender in place.  Now to add the Calendar and Event table, and fill with some sample data.

--- Website/branches/booking/app/assets/javascripts/admin/calendars.js
+++ Website/branches/booking/app/assets/javascripts/admin/calendars.js
@@ -2,5 +2,6 @@ $(document).on('page:change', function() {
 	// page is now ready, initialize the calendar...
 	$('#calendar').fullCalendar({
 		// put your options and callbacks here
+		events: '/admin/calendar.json'
 	})
 });
--- Website/branches/booking/app/controllers/admin/calendars_controller.rb
+++ Website/branches/booking/app/controllers/admin/calendars_controller.rb
@@ -5,6 +5,10 @@ class Admin::CalendarsController < ApplicationController
 	#..#
 
 	def show
+		respond_to do |format|
+			format.html {}
+			format.json { @events = [{title: 'test', start: '2015-08-01'}] }
+		end
 	end
 
 end
