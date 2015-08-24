8b279bc4c267c4a7feffa821a829f5712e215b84
randy <randy@d4c4d6a3-d3ca-4a6b-8eee-ae4fafe91092>
2015-08-24 22:39:36 +0000

    Successful testing for undefined routes!

--- Website/branches/booking/test/controllers/galleries_controller_test.rb
+++ Website/branches/booking/test/controllers/galleries_controller_test.rb
@@ -11,42 +11,45 @@ class GalleriesControllerTest < ActionController::TestCase
     assert_not_nil assigns(:galleries)
   end
 
-=begin
   test "should get new" do
     #get :new
     #assert_response :success
-		assert_throws(:"ActionController::UrlGenerationError") {get :new}
+		assert_raise(ActionController::UrlGenerationError) {get :new}
   end
 
   test "should create gallery" do
-    assert_difference('Gallery.count') do
-      post :create, gallery: {  }
-    end
-
-    assert_redirected_to gallery_path(assigns(:gallery))
+    #assert_difference('Gallery.count') do
+    #  post :create, gallery: {  }
+    #end
+    #
+    #assert_redirected_to gallery_path(assigns(:gallery))
+		assert_raise(ActionController::UrlGenerationError) {post :create, gallery: {  }}
   end
 
   test "should show gallery" do
-    get :show, id: @gallery
-    assert_response :success
+    #get :show, id: @gallery
+    #assert_response :success
+		assert_raise(ActionController::UrlGenerationError) {get :show, id: @gallery}
   end
 
   test "should get edit" do
-    get :edit, id: @gallery
-    assert_response :success
+    #get :edit, id: @gallery
+    #assert_response :success
+		assert_raise(ActionController::UrlGenerationError) {get :edit, id: @gallery}
   end
 
   test "should update gallery" do
-    patch :update, id: @gallery, gallery: {  }
-    assert_redirected_to gallery_path(assigns(:gallery))
+    #patch :update, id: @gallery, gallery: {  }
+    #assert_redirected_to gallery_path(assigns(:gallery))
+		assert_raise(ActionController::UrlGenerationError) {patch :update, id: @gallery, gallery: {  }}
   end
 
   test "should destroy gallery" do
-    assert_difference('Gallery.count', -1) do
-      delete :destroy, id: @gallery
-    end
-
-    assert_redirected_to galleries_path
+    #assert_difference('Gallery.count', -1) do
+    #  delete :destroy, id: @gallery
+    #end
+    #
+    #assert_redirected_to galleries_path
+		assert_raise(ActionController::UrlGenerationError) {delete :destroy, id: @gallery}
   end
-=end
 end
--- Website/branches/booking/test/controllers/testimonials_controller_test.rb
+++ Website/branches/booking/test/controllers/testimonials_controller_test.rb
@@ -11,41 +11,45 @@ class TestimonialsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:testimonials)
   end
 
-=begin
   test "should get new" do
-    get :new
-    assert_response :error
+    #get :new
+    #assert_response :error
+		assert_raise(ActionController::UrlGenerationError) {get :new}
   end
 
   test "should create testimonial" do
-    assert_difference('Testimonial.count') do
-      post :create, testimonial: { from: @testimonial.from, quote: @testimonial.quote }
-    end
-
-    assert_redirected_to testimonial_path(assigns(:testimonial))
+    #assert_difference('Testimonial.count') do
+    #  post :create, testimonial: { from: @testimonial.from, quote: @testimonial.quote }
+    #end
+    #
+    #assert_redirected_to testimonial_path(assigns(:testimonial))
+		assert_raise(ActionController::UrlGenerationError) {post :create, testimonial: { from: @testimonial.from, quote: @testimonial.quote }}
   end
 
   test "should show testimonial" do
-    get :show, id: @testimonial
-    assert_response :error
+    #get :show, id: @testimonial
+    #assert_response :error
+		assert_raise(ActionController::UrlGenerationError) {get :show, id: @testimonial}
   end
 
   test "should get edit" do
-    get :edit, id: @testimonial
-    assert_response :error
+    #get :edit, id: @testimonial
+    #assert_response :error
+		assert_raise(ActionController::UrlGenerationError) {get :edit, id: @testimonial}
   end
 
   test "should update testimonial" do
-    patch :update, id: @testimonial, testimonial: { from: @testimonial.from, quote: @testimonial.quote }
-    assert_redirected_to testimonial_path(assigns(:testimonial))
+    #patch :update, id: @testimonial, testimonial: { from: @testimonial.from, quote: @testimonial.quote }
+    #assert_redirected_to testimonial_path(assigns(:testimonial))
+		assert_raise(ActionController::UrlGenerationError) {patch :update, id: @testimonial, testimonial: { from: @testimonial.from, quote: @testimonial.quote }}
   end
 
   test "should destroy testimonial" do
-    assert_difference('Testimonial.count', -1) do
-      delete :destroy, id: @testimonial
-    end
-
-    assert_redirected_to testimonials_path
+    #assert_difference('Testimonial.count', -1) do
+    #  delete :destroy, id: @testimonial
+    #end
+    #
+    #assert_redirected_to testimonials_path
+		assert_raise(ActionController::UrlGenerationError) {delete :destroy, id: @testimonial}
   end
-=end
 end
