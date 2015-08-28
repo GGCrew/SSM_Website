a258db1e8ff6ff546e125b4afc9a2cd57a002f9b
randy <R_Connelie@hotmail.com>
2015-08-28 18:00:52 +0000

    Layout and CSS for events/show.html.erb Renamed fields intended for acts_as_list from "order" to "position" in doc/booking scaffold script.txt

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,5 +1,6 @@
 Time::DATE_FORMATS[:month_dd_year] = '%B %d, %Y'
 Time::DATE_FORMATS[:day] = '%A'
 Time::DATE_FORMATS[:us_date] = '%m/%d/%Y'
+Time::DATE_FORMATS[:time] = '%H:%M'
 Time::DATE_FORMATS[:timezone_abbreviation] = '%Z'
 
